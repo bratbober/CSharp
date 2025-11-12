be87f330b6140bb0253aec07d25ee6da01e6421e
p45 <p45@207-16>
2025-11-12 17:26:08 +0200

    Add project files.

---
+++ 003_Classes/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+using _003_Classes;
+
+Student student = new Student();
+
+student.SetName("john");
+student.SetName("Smith");
+student.SetAge(10);
+
+student.Print();
---
+++ 003_Classes/Student.cs
@@ -0,0 +1,21 @@
+﻿
+namespace _003_Classes
+{
+    class Student
+    {
+        private string name;
+        private string surname;
+        private int age;
+
+        public string GetName() { return name; }
+        public void SetName(string name) { this.name = name; }
+        public string GetSurname() { return surname; }
+        public void SetSurname(string surname) { this.surname = surname; }
+        public int GetAge() { return age; }
+        public void SetAge(int age) { this.age = age; }
+        public void Print()
+        {
+            Console.WriteLine($"Name: {name} | Surname: {surname} | Age: {age}");
+        }
+    }
+}
